a46796a5a07155b0ad359fe460660105a4204c89
Filip Hracek <filip.hracek@gmail.com>
2018-02-08 09:38:42 -0800

    Avoid treeshaking of benchmarks
    The counts and lengths that we’re computing are now public members on the benchmark classes so that Dart doesn’t treeshake those.

--- tool/benchmark.dart
+++ tool/benchmark.dart
@@ -1,49 +1,55 @@
 import 'package:benchmark_harness/benchmark_harness.dart';
 import 'package:english_words/english_words.dart';
 
-/// A benchmark for counting syllables.
-class SyllableBenchmark extends BenchmarkBase {
-  /// Create the benchmark.
-  SyllableBenchmark() : super("Syllable");
-
-  /// Run and report on the benchmark.
-  static void main() {
-    new SyllableBenchmark().report();
-  }
-
-  final List<String> _words = all.take(1000).toList(growable: false);
-
-  /// Count cumulative number of syllables in [_words].
-  @override
-  void run() {
-    int count = 0;
-    for (var word in _words) {
-      count += syllables(word);
-    }
-  }
+void main() {
+  SyllableBenchmark.main();
+  GenerateWordPairBenchmark.main();
 }
 
 /// A benchmark for counting syllables.
 class GenerateWordPairBenchmark extends BenchmarkBase {
+  /// This lives here so that Dart doesn't tree-shake the computation in the
+  /// benchmark.
+  int length = 0;
+
   /// Create the benchmark.
   GenerateWordPairBenchmark() : super("GenerateWordPair");
+
+  /// Generate 100 random [WordPair]s and count cumulative length.
+  @override
+  void run() {
+    for (var pair in generateWordPairs().take(100)) {
+      length += pair.first.length + pair.second.length;
+    }
+  }
 
   /// Run and report on the benchmark.
   static void main() {
     new GenerateWordPairBenchmark().report();
   }
-
-  /// Generate 100 random [WordPair]s and count cumulative length.
-  @override
-  void run() {
-    int length = 0;
-    for (var pair in generateWordPairs().take(100)) {
-      length += pair.first.length + pair.second.length;
-    }
-  }
 }
 
-void main() {
-  SyllableBenchmark.main();
-  GenerateWordPairBenchmark.main();
+/// A benchmark for counting syllables.
+class SyllableBenchmark extends BenchmarkBase {
+  final List<String> _words = all.take(1000).toList(growable: false);
+
+  /// This lives here so that Dart doesn't tree-shake the computation in the
+  /// benchmark.
+  int count = 0;
+
+  /// Create the benchmark.
+  SyllableBenchmark() : super("Syllable");
+
+  /// Count cumulative number of syllables in [_words].
+  @override
+  void run() {
+    for (var word in _words) {
+      count += syllables(word);
+    }
+  }
+
+  /// Run and report on the benchmark.
+  static void main() {
+    new SyllableBenchmark().report();
+  }
 }
